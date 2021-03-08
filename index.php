54329d3dc3d85f2bed1a7871e5863a41b179d505
douglas254 <dobara@kabarak.ac.ke>
2021-03-08 22:47:16 +0300

    ecommerce website startup homepage

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Douglas Obara">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Ecommerce &ndash; Website</title>
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <div class="logo">
+                <a href="#"><img src="images/ecommerce-logo.png" alt="ecommerce logo" style="width:30%;height:120px;border: 5px solid black;border-radius:60%;"/><a/>
+            </div>   
+            <nav class="navbar">
+                <ul>
+                    <li><a href="" id="active"><i class="icon-home"></i>Home</a></li>
+                    <li><a href=""><i class="icon-cog"></i>Services</a></li>
+                    <li><a href=""><i class="icon-phone"></i>Contact us</a></li>
+                    <button type="button" class="login-reg-btn"><i class="icon-user"></i><a href="login.php"> Log In</a></button>
+                    <button class="login-reg-btn"><a href="#"><i class="icon-shopping-cart" style="font-size:15px;margin-left:10px;color:black"></i> Cart</a></button>
+                </ul>
+            </nav>            
+        </header>
+    
+        <footer>
+            <a href="" ><center>Douglas254</a> &copy; 2021</center>
+        </footer>
+    </div>
+
+</body>
+</html>
