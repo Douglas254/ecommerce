5d844fcd2f6a11051f5450073536926f9e4e38fa
douglas254 <dobara@kabarak.ac.ke>
2021-03-08 22:50:29 +0300

    login page

---
+++ login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login Page</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
+    </head>
+    <body>
+        <div class="container">
+            <div class="row col-md-6 col-md-offset-3">
+                <div class="panel panel-primary">
+                    <div class="panel-heading text-center">
+                        <h1>Login Form</h1>
+                    </div>
+                    <div class="panel-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" id="username" name="Username"/>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" id="password" name="Password"/>
+                            </div>
+                                <input type="submit" value="Login" class="btn btn-primary" />
+                        </form>
+                    </div>
+                    <div class="panel-footer text-right">
+                        <center><b>If new register <a href="registration.html">here</a></b></center> </br> <small>Douglas254 &copy; Shopping Mall</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
